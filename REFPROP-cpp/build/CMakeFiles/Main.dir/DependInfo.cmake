2d08139ad73acdc727e601402602dd909b40130d
A.J. Uppal <ajuppal@AJs-MacBook-Pro-2.local>
2020-10-20 21:14:11 -0400

    cpp directory

---
+++ REFPROP-cpp/build/CMakeFiles/Main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "X:/CoBraPython/REFPROP-cpp/main.cpp" "X:/CoBraPython/REFPROP-cpp/build/CMakeFiles/Main.dir/main.cpp.obj"
+  "X:/CoBraPython/REFPROP-cpp/other.cpp" "X:/CoBraPython/REFPROP-cpp/build/CMakeFiles/Main.dir/other.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ REFPROP-cpp/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	std::cout << (true ? "Hello" : "World");
+	return 0;
+}
